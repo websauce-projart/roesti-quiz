d8f007ab2f29013a6a5bd557547aa5e7f81dce9f
Thomas Robert <thomas.robert@heig-vd.ch>
2021-05-28 10:56:28 +0200

    Add basic structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,9 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
