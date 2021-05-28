354e51e4fade753d108cea298efa6efc536cd7b3
Thomas Robert <thomas.robert@heig-vd.ch>
2021-05-28 11:36:03 +0200

    Add postcss

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	plugins: [
+		[
+			"postcss-preset-env",
+			{
+				browsers: "last 2 versions",
+				stage: 1
+			}
+		]
+	]
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js("resources/js/app.js", "public/js")
+    .vue()
+    .sourceMaps()
+    .browserSync({
+        port: 8000,
+        open: false,
+        notify: false
+    })
+    .postCss("resources/css/app.css", "public/css");
