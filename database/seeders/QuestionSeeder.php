3562252c96ec40afc2958c67fe5cec055a136d52
Thomas Robert <thomas.robert@heig-vd.ch>
2021-06-02 12:04:43 +0200

    Update question seeder and factory

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -2,38 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\Question;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class QuestionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $arrayQuestions = [
-            'Blablablabal' => 0,
-            'onaodn' => 1,
-            'HeyHey' => 1,
-            'oadoandoa' => 0,
-            'oqoqopmpca' => 0,
-            'oamdoamo' => 0,
-            'nondpqmdpma' => 1,
-            'omcomomdowd' => 0,
-            'qomdpqmdpm' => 1,
-        ];
-
-        foreach($arrayQuestions as $question => $answer ){
-            DB::table('questions')->insert([
-                'label' => $question,
-                'correct_answer' => $answer,
-                'created_at' => now(),
-                'updated_at' => now(),
-                'id_author' => rand(1,2)
-            ]);
-        };  
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Question::factory()
+			->count(10)
+			->create();
+	}
 }
