95accde99cbc586fc538ca517d4e74ec743eab37
Luka Najjar <coksilaweed@gmail.com>
2021-06-03 18:46:30 +0200

    Update components

---
+++ app/View/Components/InputText.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class InputText extends Component
+{
+    public $label;
+    public $id;
+    public $placeholder;
+    public $icon;
+    public $type;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($label, $id, $placeholder, $icon="")
+    {
+        $this->label = $label;
+        $this->id = $id;
+        $this->placeholder = $placeholder;
+        $this->icon = $icon;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.input-text');
+    }
+}
