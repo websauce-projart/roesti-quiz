0ff786d3701d44b948b60e2b17c583e0b0d4e569
Luka Najjar <coksilaweed@gmail.com>
2021-06-08 13:20:37 +0200

    Fix routes bug

--- resources/views/gameloop/results.blade.php
+++ resources/views/gameloop/results.blade.php
@@ -8,7 +8,7 @@
 
     {{ $user->pseudo }} vs. {{ $opponent->pseudo }}<br>
     @foreach ($rounds as $round)
-        <form action="{{ route('round_history') }}" method="POST">
+        <form action="" method="POST">
             @csrf
 
             <input type="hidden" name="round_id" value="{{ $round['id'] }}">
