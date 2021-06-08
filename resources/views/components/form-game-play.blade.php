a42206c71ac45177998e58611e2594ccb924c81c
Karim Zerika <karim.zerika@heig-vd.ch>
2021-06-08 12:00:09 +0200

    adapt-routes

--- resources/views/components/form-game-play.blade.php
+++ resources/views/components/form-game-play.blade.php
@@ -2,7 +2,7 @@
 <div class="home--game">
     <span class="home--game--pseudo">{{$pseudo}}</span>
     <span class="home--game--info">À toi de jouer</span>
-    <form method="POST" action="{{ route('getresults') }}" accept-charset="UTF-8">
+    <form method="POST" action="{{ route('redirectFromHome') }}" accept-charset="UTF-8">
         @csrf
         <input type="hidden" name="game_id" value="{{$value}}">
         <div class="home--game--play">
--- resources/views/components/form-game-wait.blade.php
+++ resources/views/components/form-game-wait.blade.php
@@ -2,7 +2,7 @@
 <div class="home--game home--game--wait">
     <span class="home--game--pseudo">{{$pseudo}}</span>
     <span class="home--game--info">N'a pas encore relevé ton défi...</span>
-    <form method="POST" action="{{ route('getresults') }}" accept-charset="UTF-8">
+    <form method="POST" action="{{ route('redirectFromHome') }}" accept-charset="UTF-8">
         @csrf
         <input type="hidden" name="game_id" value="{{$value}}">
         <input class = "home--game--submit" type="submit" value="">
