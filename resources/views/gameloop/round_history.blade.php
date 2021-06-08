9d9be2a12452e6c43d090e1c9ee633236c8b2b9b
Thomas Robert <thomas.robert@heig-vd.ch>
2021-06-08 22:33:37 +0200

    Fix history_round back link route

--- resources/views/gameloop/round_history.blade.php
+++ resources/views/gameloop/round_history.blade.php
@@ -6,44 +6,43 @@
 
 @section('content')
     <h1>Historique des réponses</h1>
-    {{-- <a href="{{ route('results'), [$game_id] }}"> --}}
-    <a href="#">< back</a>
+    <a href="{{ route('join', $game_id) }}">Retour</a>
 
-            @foreach ($questions as $question)
-                <div class="card">
+    @foreach ($questions as $question)
+        <div class="card">
 
-                    <div>
-                        {{ $user['object']->pseudo }} a répondu:
-                        @isset($user['answers'])
-                            @if ($user['answers'][$loop->index]['user_answer'])
-                                vrai
-                            @else
-                                faux
-                            @endif
-                        @endisset
+            <div>
+                {{ $user['object']->pseudo }} a répondu:
+                @isset($user['answers'])
+                    @if ($user['answers'][$loop->index]['user_answer'])
+                        vrai
+                    @else
+                        faux
+                    @endif
+                @endisset
 
-                    </div>
+            </div>
 
-                    <div>
-                        {{ $question->label }}
-                        @if ($question->answer_label)
-                            <br><small>{{ $question->answer_label }}</small>
-                        @endif
-                    </div>
+            <div>
+                {{ $question->label }}
+                @if ($question->answer_label)
+                    <br><small>{{ $question->answer_label }}</small>
+                @endif
+            </div>
 
-                    <div>
-                        {{ $opponent['object']->pseudo }}
-                        @isset($opponent['answers'])
-                            @if ($opponent['answers'][$loop->index]['user_answer'])
-                                vrai
-                            @else
-                                faux
-                            @endif
-                        @endisset
-                    </div>
+            <div>
+                {{ $opponent['object']->pseudo }}
+                @isset($opponent['answers'])
+                    @if ($opponent['answers'][$loop->index]['user_answer'])
+                        vrai
+                    @else
+                        faux
+                    @endif
+                @endisset
+            </div>
 
 
-                </div>
-            @endforeach
+        </div>
+    @endforeach
 
-        @endsection
+@endsection
