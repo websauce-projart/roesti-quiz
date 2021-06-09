00b1aa5a7f8ba7fb82c7f88febed9da546e2cc3e
Thomas Robert <thomas.robert@heig-vd.ch>
2021-06-09 13:56:35 +0200

    Update form elements styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,76 +6,82 @@
 
 @section('content')
 
-    <body class="container">
-        <header class="header--login">
-            <nav class="topnav topnav--right">
-                <a href="{{ route('register') }}">Créer un compte</a>
-            </nav>
-            <img class="logo" src="img/logo_v2_1.svg" alt="Rösti Quiz" />
-        </header>
+    <body>
+        <div class="container">
+            <header class="header--login">
+                <nav class="topnav topnav--right">
+                    <a href="{{ route('register') }}">Créer un compte</a>
+                </nav>
+                <img class="logo" src="img/logo_v2_1.svg" alt="Rösti Quiz" />
+            </header>
 
-        <main>
-            <form action="" method="post">
-                @csrf
+            <main>
+                <form action="" method="post" class="form form--center">
+                    @csrf
 
-                @if (Session::has('account-deleted'))
-                    {{ Session::get('account-deleted') }}
-                @endif
-                <div class="center">
-                    <x-input-text label="Pseudo ou email" id="pseudo" placeholder="Entrez votre pseudo ou votre email"
-                        icon="😃" value="{{ old('pseudo') }}"></x-input-text>
-                    {!! $errors->first('pseudo', '<small class="help-block">:message</small>') !!}
-                </div>
+                    @if (Session::has('account-deleted'))
+                        {{ Session::get('account-deleted') }}
+                    @endif
 
-                <div class="center">
-                    <x-input-text label="Mot de passe" id="password" placeholder="Entrez votre pseudo..." icon="🔒"
-                        value="{{ old('password') }}" type="password"></x-input-text>
-                    {!! $errors->first('password', '<small class="help-block">:message</small>') !!}
-                </div>
+                    <div class="form__row">
+                        <x-input-text label="Pseudo ou email" id="pseudo" placeholder="Entrez votre pseudo ou votre email"
+                            icon="😃" value="{{ old('pseudo') }}"></x-input-text>
+                        {!! $errors->first('pseudo', '<small class="help-block">:message</small>') !!}
+                    </div>
 
-                <div class="checkbox center">
-                    <input type="checkbox" id="remember_token" name="remember_token" value="true">
-                    <label for="remember_token">Se souvenir de moi ?</label>
-                </div>
+                    <div class="form__row">
+                        <x-input-text label="Mot de passe" id="password" placeholder="Entrez votre pseudo..." icon="🔒"
+                            value="{{ old('password') }}" type="password"></x-input-text>
+                        {!! $errors->first('password', '<small class="help-block">:message</small>') !!}
+                    </div>
+
+                    <div class="form__row form__row--left">
+                        <input type="checkbox" id="remember_token" name="remember_token" value="true">
+                        <label for="remember_token">Se souvenir de moi ?</label>
+                    </div>
+
+                    <div class="form__row">
+                        <x-input-submit label="Se connecter"></x-input-submit>
+                    </div>
+
+                    <div class="form__row">
+                        <a href="{{ route('password.request') }}">Mot de passe oublié?</a>
+                    </div>
+
+                </form>
 
                 <div>
-                    <x-input-submit label="Se connecter"></x-input-submit>
-                    <a href="{{ route('password.request') }}">Mot de passe oublié?</a>
+                    @if (Session::has('account-created'))
+                        {{ Session::get('account-created') }}
+                    @endif
+
+                    @if ($errors->has('loginFailed'))
+                        😔 Désolé, le pseudo ou le mot de passe n'est pas correct 😔
+                    @endif
+
+                    @if (Session::has('account-verified'))
+                        {{ Session::get('account-verified') }}
+                    @endif
+
+                    @if ($errors->has('account-verify'))
+                        ⚠️ stp confirme ton mail ⚠️
+
+                        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                            @csrf
+                            <button type="submit" class="btn btn--primary btn-link p-0 m-0 align-baseline">si ta perdu ton
+                                mail
+                                clic
+                                ici
+                                lol</button>
+                        </form>
+                    @endif
+
+                    @if (session('email-resent'))
+                        {{ Session::get('email-resent') }}
+                    @endif
                 </div>
-
-            </form>
-
-            <div>
-                @if (Session::has('account-created'))
-                    {{ Session::get('account-created') }}
-                @endif
-
-                @if ($errors->has('loginFailed'))
-                    😔 Désolé, le pseudo ou le mot de passe n'est pas correct 😔
-                @endif
-
-                @if (Session::has('account-verified'))
-                    {{ Session::get('account-verified') }}
-                @endif
-
-                @if ($errors->has('account-verify'))
-                    ⚠️ stp confirme ton mail ⚠️
-
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn--primary btn-link p-0 m-0 align-baseline">si ta perdu ton mail
-                            clic
-                            ici
-                            lol</button>
-                    </form>
-                @endif
-
-                @if (session('email-resent'))
-                    {{ Session::get('email-resent') }}
-                @endif
-            </div>
-        </main>
-
+            </main>
+        </div>
     </body>
 
 @endsection
