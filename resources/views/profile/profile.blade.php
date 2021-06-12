f9685850de110d989155a34592b178051605c094
Thomas Robert <thomas.robert@heig-vd.ch>
2021-06-12 12:07:59 +0200

    Add style to profile page

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -5,34 +5,42 @@ Profil
 @endsection
 
 @section('content')
-<div>
-    <x-avatar 
-    posePath="{{$data['user']->getPosePath()}}"
-    eyePath="{{$data['user']->getMouthPath()}}"
-    mouthPath="{{$data['user']->getEyePath()}}"
-    ></x-avatar>
-    <a href="{{route('updateAvatar', [$data['user']->id])}}">Edit avatar</a>
-</div>
-</br>
-<div>
-    {{$data['user']->pseudo}}
-    </br>
-    {{$data['score']['titleScore']}} - {{$data['score']['points']}} points
-</div>
-</br>
-<div>
-    <a href="{{route('updatePasswordForm', [$data['user']->id])}}">Modifier mon mot de passe</a></li>
-    </br>
-    <form class="form-horizontal" method="post" action="" onsubmit="return confirm('Voulez-vous vraiment supprimer votre compte ?');">
-        @csrf
-        <input type="submit" value="Supprimer mon compte">
-    </form>
-</div>
-</br>
-<div>
-    Mes trophées
+<div class="container">
 
-    Liste des trophées
-</div>
+	<header class="header header--home">
+		<nav class="topnav">
+			<a href="{{ route('home') }}" class="icon-arrow-left" aria-label="Retour"></a>
+		</nav>
+		<img class="logo" src="{{ asset("img/logo_v2_1.svg") }}" alt="Rösti Quiz" />
+	</header>
 
+	<main class="profile">
+		<section class="profile__avatar">
+			<div class="profile__avatar__image">
+				<x-avatar posePath="{{$data['user']->getPosePath()}}" eyePath="{{$data['user']->getMouthPath()}}" mouthPath="{{$data['user']->getEyePath()}}"></x-avatar>
+
+				<div class="profile__avatar__editButton">
+					<a href="{{route('updateAvatar', [$data['user']->id])}}" class="icon-edit-pencil" aria-label="Éditer l'avatar"></a>
+				</div>
+
+			</div>
+		</section>
+
+		<section class="profile__info">
+			<h1 class="profile__info__pseudo">{{$data['user']->pseudo}}</h1>
+			<div class="profile__info__score">{{$data['score']['titleScore']}} · {{$data['score']['points']}} points</div>
+		</section>
+
+		<section class="profile__actions">
+			<a class="btn btn--tertiary" href="{{ route('updatePasswordForm', [$data['user']->id]) }}">Changer de mot de passe</a>
+			<a class="btn btn--tertiary" href="{{ route("logout") }}">Se déconnecter</a>
+			<form method="post" action="" onsubmit="return confirm('Voulez-vous vraiment supprimer votre compte ?');">
+				@csrf
+				<button type="submit">Supprimer mon compte</button>
+			</form>
+		</section>
+
+	</main>
+
+</div>
 @endsection
