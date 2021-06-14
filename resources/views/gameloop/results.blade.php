bf0b738740096d7b9d400bfea46a3d2a875e88af
Thomas Robert <thomas.robert@heig-vd.ch>
2021-06-14 09:20:05 +0200

    Add disable state for current round

--- resources/views/gameloop/results.blade.php
+++ resources/views/gameloop/results.blade.php
@@ -46,27 +46,37 @@ pattern-body-fix
     <section class="roundBadge__container">
         @foreach ($rounds as $round)
 
-        <a class="roundBadge" data-index="{{ count($rounds) - $loop->index }}"
-            href="{{ route('round_history', [$game, $round->id]) }}">
+        <a
+		  	class="roundBadge
+		  	{{
+			  	$game->active_user_id == $user->id &&
+				$lastRound->id == $round->id &&
+				$round->results()->get()->count() != 2
+				? 'disabled' : ''
+			}}"
+			data-index="{{ count($rounds) - $loop->index }}"
+         href="{{ route('round_history', [$game, $round->id]) }}"
+			>
+
             <div class="roundBadge__score">
                 @if ($game->active_user_id == $user->id &&
                 $lastRound->id == $round->id &&
                 $round->results()->get()->count() != 2)
-                À ton tour
+                	À ton tour
                 @elseif($game->active_user_id == $user->id && $lastRound->id == $round->id &&
                 $round->results()->get()->count() == 2)
-                {{ $round->getScore($user->id) }}
+               	 {{ $round->getScore($user->id) }}
                 @elseif($game->active_user_id != $user->id && $lastRound->id == $round->id &&
                 $round->results()->get()->count() == 0)
-                En attente
+               	 En attente
                 @elseif($game->active_user_id != $user->id && $lastRound->id == $round->id &&
                 $round->results()->get()->count() > 0)
-                {{ $round->getScore($user->id) }}
+               	 {{ $round->getScore($user->id) }}
                 @else
-                {{ $round->getScore($user->id) }}
+               	 {{ $round->getScore($user->id) }}
                 @endif
-
             </div>
+
             <div class="roundBadge__category">
                 {{ $round->getCategory()->title }}
             </div>
