6f06f48b293faeadf9b24f73dd4c5ea486445f42
Thomas Robert <thomas.robert@heig-vd.ch>
2021-06-14 15:41:40 +0200

    Add page errors

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,27 @@
-@extends('errors::minimal')
+@extends('template')
 
-@section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('title', "Accès interdit")
+
+@push('body-classes', "bg--white")
+
+@section('content')
+<div class="container speech-bubble">
+
+	<div class="errorPage">
+		<h1>Erreur 403</h1>
+
+		<div class="errorPage--message">
+			La page demandée t'es inaccessible. <br>
+			Désolé mon p’tit rösti !
+		</div>
+
+		<div class="bottombar">
+			<a href="{{ route("login") }}" class="btn btn--primary">Retour à l'accueil</a>
+		</div>
+	</div>
+
+</div>
+
+<x-presenter mood="404"></x-presenter>
+
+@endsection
