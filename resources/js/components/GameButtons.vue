fe4d2c74b791c806e7cd38da23b63c6cd8abd2fd
Steve Mettraux <steve.mettraux@heig-vd.ch>
2021-06-14 21:14:38 +0200

    keypress is working again

--- resources/js/components/GameButtons.vue
+++ resources/js/components/GameButtons.vue
@@ -12,15 +12,49 @@
 </template>
 
 <script>
+import { useKeypress } from 'vue3-keypress'
+import {ref} from "vue";
 export default {
   name: "GameButtons",
-
 	// props:{
 	// 	qid:{
 	// 	 type: Number,
 	// 	 required: true,
 	//  }
 	// },
+
+	setup(props, {emit}) {
+
+    const someSuccessCallback = ({ keyCode }) => {
+		 if(keyCode===37){
+			 console.log("gauche");
+			emit("reject")
+
+		 } else if (keyCode === 39){
+			 console.log("droite");
+			 emit("accept")
+
+		 }
+
+    };
+
+
+    useKeypress({
+      keyEvent: "keydown",
+      keyBinds: [
+        {
+          keyCode: "left", // or keyCode as integer, e.g. 37
+          success: someSuccessCallback,
+			 preventDefault: true, // the default is true
+        },
+		   {
+          keyCode: "right", // or keyCode as integer, e.g. 39
+          success: someSuccessCallback,
+          preventDefault: true, // the default is true
+        },
+      ],
+    });
+  },
 
   methods: {
     buttonRejected() {
