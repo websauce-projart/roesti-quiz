096bad862a3d11c8fc52e473e279661e96c19ab2
Luka Najjar <coksilaweed@gmail.com>
2021-06-15 10:41:52 +0200

    Fix scrollbars

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,79 +1,81 @@
 <template>
-	<div class="gameBadge__container" v-if="datas.length == 0">
-		<div class="gameBadge__noGame">
-			<p>Tu n'as encore aucune partie en cours.</p>
-			<p>Pour un max de fun, clique sur <mark>nouvelle partie</mark></p>
-		</div>
-	</div>
-	<div class="gameBadge__container" v-else>
-		<h1 class="gameBadge__title" v-if="datas.length > 1">Parties en cours</h1>
-		<h1 class="gameBadge__title" v-else>Partie en cours</h1>
+  <div class="gameBadge__container" v-if="datas.length == 0">
+    <div class="gameBadge__noGame">
+      <p>Tu n'as encore aucune partie en cours.</p>
+      <p>Pour un max de fun, clique sur <mark>nouvelle partie</mark></p>
+    </div>
+  </div>
+  <div class="gameBadge__container" v-else>
+    <div>
+      <h1 class="gameBadge__title" v-if="datas.length > 1">Parties en cours</h1>
+      <h1 class="gameBadge__title" v-else>Partie en cours</h1>
 
-		<div class="gameBadge--wrapper" v-for="data in datasOrderd" :key="data">
-			<game-home :data="data"></game-home>
-		</div>
+      <div class="gameBadge--wrapper" v-for="data in datasOrderd" :key="data">
+        <game-home :data="data"></game-home>
+      </div>
 
-		<div class="gameBadge--phantom"></div>
-	</div>
+      <div class="gameBadge--phantom"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 import gameHome from "../components/gameHome.vue";
 
 export default {
-	components: { gameHome },
+  components: { gameHome },
 
-	data() {
-		return {
-			datas: {},
-			datasOrderd: [],
-			changedData: true,
-		};
-	},
+  data() {
+    return {
+      datas: {},
+      datasOrderd: [],
+      changedData: true,
+    };
+  },
 
-	props: {
-		data_url: String,
-	},
+  props: {
+    data_url: String,
+  },
 
-	setup(props) {},
+  setup(props) {},
 
-	mounted: function () {
-		this.orderData();
-	},
+  mounted: function () {
+    this.orderData();
+  },
 
-	created() {
-		if (this.changedData)
-			this.interval = setInterval(() => this.orderData(), 5000);
-	},
+  created() {
+    if (this.changedData)
+      this.interval = setInterval(() => this.orderData(), 5000);
+  },
 
-	methods: {
-		async orderData() {
-			await this.fetchText();
+  methods: {
+    async orderData() {
+      await this.fetchText();
 
-			let newData = [];
-			for (var index in this.datas) {
-				if (
-					this.datas[index].user.id ==
-					this.datas[index].game.active_user_id
-				) {
-					newData.unshift(this.datas[index]);
-				} else {
-					newData.push(this.datas[index]);
-				}
-			}
-			if(JSON.stringify(this.datasOrderd) != JSON.stringify(newData)) this.datasOrderd = newData;
-		},
+      let newData = [];
+      for (var index in this.datas) {
+        if (
+          this.datas[index].user.id == this.datas[index].game.active_user_id
+        ) {
+          newData.unshift(this.datas[index]);
+        } else {
+          newData.push(this.datas[index]);
+        }
+      }
+      if (JSON.stringify(this.datasOrderd) != JSON.stringify(newData))
+        this.datasOrderd = newData;
+    },
 
-		fetchText() {
-			return axios
-				.get(this.$props.data_url)
-				.then((response) => {
-					if (this.datas.length == response.data.length)
-						this.changedData == false;
-					this.datas = response.data;
-				})
-				.catch((errors) => console.log(errors));
-		},
-	},
+    fetchText() {
+      return axios
+        .get(this.$props.data_url)
+        .then((response) => {
+          if (this.datas.length == response.data.length)
+            this.changedData == false;
+          this.datas = response.data;
+        })
+        .catch((errors) => console.log(errors));
+    },
+  },
 };
 </script>
--- resources/js/pages/new_game.vue
+++ resources/js/pages/new_game.vue
@@ -1,53 +1,59 @@
 <template>
-	<div class="form__row">
-		<div class="form__field">
-			<input
-				type="search"
-				placeholder="Recherche un ami…"
-				v-model="searchText"
-			/>
-			<i class="icon-search"></i>
-		</div>
-	</div>
+  <div class="form__row">
+    <div class="form__field">
+      <input
+        type="search"
+        placeholder="Recherche un ami…"
+        v-model="searchText"
+      />
+      <i class="icon-search"></i>
+    </div>
+  </div>
 
-	<div class="searchFriends__container">
-		<ul class="searchFriends">
-			<li v-for="data in filteredList()" :key="data">
-				<input
-					@click="playerSelected()"
-					type="radio"
-					:id="`${data}`"
-					name="user"
-					:value="`${data}`"
-				/>
-				<label :for="`${data}`">
-					<div class="searchFriends__avatar">
-						<avatar :dataSearch="this.findId(data)"></avatar>
-					</div>
+  <div class="searchFriends__container">
+    <ul class="searchFriends">
+      <div>
+        <li v-for="data in filteredList()" :key="data">
+          <input
+            @click="playerSelected()"
+            type="radio"
+            :id="`${data}`"
+            name="user"
+            :value="`${data}`"
+          />
+          <label :for="`${data}`">
+            <div class="searchFriends__avatar">
+              <avatar :dataSearch="this.findId(data)"></avatar>
+            </div>
 
-					{{ data }}
-				</label>
-			</li>
-		</ul>
-	</div>
+            {{ data }}
+          </label>
+        </li>
+      </div>
+    </ul>
+  </div>
 
-	<div class="searchFriends--noFriend" v-if="showRandomPlayer()" :key="searchText">
-		<div class="searchFriends__title">Tu n'as pas d'ami ?</div>
+  <div
+    class="searchFriends--noFriend"
+    v-if="showRandomPlayer()"
+    :key="searchText"
+  >
+    <div class="searchFriends__title">Tu n'as pas d'ami ?</div>
 
-		<div class="btn btn--secondary" @click="this.randomPlayer()">
-			Victime aléatoire
-		</div>
-	</div>
+    <div class="btn btn--secondary" @click="this.randomPlayer()">
+      Victime aléatoire
+    </div>
+  </div>
 
-	<div class="bottombar">
-		<input
-			v-if="this.returnValue"
-			class="btn btn--primary"
-			id="submit--new--game"
-			type="submit"
-			value="Suivant"
-		/>
-	</div>
+  <div class="bottombar">
+    <input
+      v-if="this.returnValue"
+      class="btn btn--primary"
+      id="submit--new--game"
+      type="submit"
+      value="Suivant"
+    />
+  </div>
 </template>
 
 <script>
@@ -55,101 +61,101 @@ import { ref } from "vue";
 import avatar from "../elements/show_avatar.vue";
 
 export default {
-	props: {
-		datas: Object,
-	},
+  props: {
+    datas: Object,
+  },
 
-	components: {
-		avatar,
-	},
+  components: {
+    avatar,
+  },
 
-	data() {
-		return {
-			randomPlayerPseudo: "",
-			returnValue: false,
-			randomActualise: false,
-		};
-	},
+  data() {
+    return {
+      randomPlayerPseudo: "",
+      returnValue: false,
+      randomActualise: false,
+    };
+  },
 
-	setup(props) {
-		let searchText = ref("");
+  setup(props) {
+    let searchText = ref("");
 
-		let list = [];
+    let list = [];
 
-		//Show or hide the button for a random player depending on the input text value
-		function showRandomPlayer() {
-			if (searchText.value == "") return true;
-			else {
-				return false;
-			}
-		}
+    //Show or hide the button for a random player depending on the input text value
+    function showRandomPlayer() {
+      if (searchText.value == "") return true;
+      else {
+        return false;
+      }
+    }
 
-		//Filter the list of User with the input box and returns an array of corresponding users
-		function filteredList() {
-			list = [];
-			let datas = props.datas;
-			if (searchText.value == "") {
-				return list;
-			}
+    //Filter the list of User with the input box and returns an array of corresponding users
+    function filteredList() {
+      list = [];
+      let datas = props.datas;
+      if (searchText.value == "") {
+        return list;
+      }
 
-			this.playerSelected;
+      this.playerSelected;
 
-			for (let index in datas) {
-				list.push(datas[index].pseudo);
-			}
+      for (let index in datas) {
+        list.push(datas[index].pseudo);
+      }
 
-			return list.filter((data) =>
-				data.toLowerCase().includes(searchText.value.toLowerCase())
-			);
-		}
+      return list.filter((data) =>
+        data.toLowerCase().includes(searchText.value.toLowerCase())
+      );
+    }
 
-		return { searchText, filteredList, showRandomPlayer };
-	},
-	methods: {
-		//Return a random player from the available ones, show it in the input text and selects it
-		randomPlayer() {
-			this.playerSelected;
-			var keys = Object.keys(this.$props.datas);
-			this.randomPlayerPseudo =
-				this.$props.datas[keys[(keys.length * Math.random()) << 0]].pseudo;
-			this.searchText = this.randomPlayerPseudo;
-			this.randomActualise = true;
-			return this.randomPlayerPseudo;
-		},
+    return { searchText, filteredList, showRandomPlayer };
+  },
+  methods: {
+    //Return a random player from the available ones, show it in the input text and selects it
+    randomPlayer() {
+      this.playerSelected;
+      var keys = Object.keys(this.$props.datas);
+      this.randomPlayerPseudo =
+        this.$props.datas[keys[(keys.length * Math.random()) << 0]].pseudo;
+      this.searchText = this.randomPlayerPseudo;
+      this.randomActualise = true;
+      return this.randomPlayerPseudo;
+    },
 
-		//Return a true value if a player is selected
-		playerSelected() {
-			this.returnValue = false;
-			document.querySelectorAll("li").forEach((element) => {
-				if (element.querySelector("input").checked) {
-					this.returnValue = true;
-				}
-			});
-		},
+    //Return a true value if a player is selected
+    playerSelected() {
+      this.returnValue = false;
+      document.querySelectorAll("li").forEach((element) => {
+        if (element.querySelector("input").checked) {
+          this.returnValue = true;
+        }
+      });
+    },
 
-		findId(pseudo) {
-			if (typeof pseudo == "string") {
-				return this.$props.datas.find((e) => e.pseudo == pseudo).id;
-			}
-		},
-	},
-	updated() {
-		this.findId(this.$props.datas);
-		//after datas and DOM are updated
-		this.$nextTick(function () {
-			//Hide the submit button if searchText is empty
-			if (this.searchText == "") {
-				this.returnValue = false;
-				this.randomPlayerPseudo = "";
-			}
+    findId(pseudo) {
+      if (typeof pseudo == "string") {
+        return this.$props.datas.find((e) => e.pseudo == pseudo).id;
+      }
+    },
+  },
+  updated() {
+    this.findId(this.$props.datas);
+    //after datas and DOM are updated
+    this.$nextTick(function () {
+      //Hide the submit button if searchText is empty
+      if (this.searchText == "") {
+        this.returnValue = false;
+        this.randomPlayerPseudo = "";
+      }
 
-			//Checks the randomPlayer input radio
-			if (this.randomPlayerPseudo != "" && this.randomActualise) {
-				document.getElementById(this.randomPlayerPseudo).checked = true;
-				this.randomActualise = false;
-				this.returnValue = true;
-			}
-		});
-	},
+      //Checks the randomPlayer input radio
+      if (this.randomPlayerPseudo != "" && this.randomActualise) {
+        document.getElementById(this.randomPlayerPseudo).checked = true;
+        this.randomActualise = false;
+        this.returnValue = true;
+      }
+    });
+  },
 };
 </script>
