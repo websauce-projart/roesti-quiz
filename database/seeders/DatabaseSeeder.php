75dda2d886a0bc4d7592efd76a10520d7f1652da
Luka Najjar <coksilaweed@gmail.com>
2021-06-16 20:17:04 +0200

    Remove unused imports

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Game;
-use App\Models\User;
-use App\Models\Round;
-use App\Models\Result;
-use App\Models\Category;
-use App\Models\Question;
-use App\Models\UserAnswer;
 use Illuminate\Database\Seeder;
 use Database\Seeders\GameSeeder;
 use Database\Seeders\UserSeeder;
