67c4195501e76c2add8a0c4589e4377a09ad4cce
Luka Najjar <coksilaweed@gmail.com>
2021-06-17 15:03:29 +0200

    Update mail image URL

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://i.imgur.com/ZccTVwN.png" class="logo" alt="RöstiQuiz Logo">
+<img src="https://pingouin.heig-vd.ch/websauce/img/logo_v2_1.svg" class="logo" alt="RöstiQuiz Logo">
 @else
 {{ $slot }}
 @endif
